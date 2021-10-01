12a9d52226d9b87fcc7acffa4872a7e21c3d7665
isis <icey1727@gmail.com>
2021-10-01 11:41:55 -0400

    adds ability to see all movies from movies api, adds create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movies.id">
+      <h2>Movie: {{ movie.title }} {{ movie.year }}</h2>
+    </div>
+    <div>
+      Title:
+      <input type="text" v-model="newMovie.title" />
+    </div>
+    <div>
+      Year:
+      <input type="text" v-model="newMovie.year" />
+    </div>
+    <button v-on:click="createMovie()">Create Movie</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+      newMovie: {},
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/one_movie").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: "Onward",
+        year: 2020,
+      };
+      axios.post("http://localhost:3000/one_movie", params).then((response) => {
+        console.log(response.data);
+        this.movies.push = response.data;
+      });
+    },
   },
 };
 </script>
